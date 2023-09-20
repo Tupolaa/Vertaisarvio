146eb8a0296a1e8749f9e16b367c3844f842ff53
Teemu eemeli Tupola <Teemu.tupola@student.hamk.fi>
2023-09-20 15:35:39 +0300

    Tietojen kysyminen
    Kysyy tiedot ja lopettaa ohjelman, jos joku tiedoista puuttuu.

--- src/Login.java
+++ src/Login.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String[] vastaus = new String[3];
+		vastaus[0] = "0";
+		vastaus[1] = "0";
+		vastaus[2] = "0";
+System.out.println("Etunimi?");
+//vastaus1 = in.nextLine();
 
+	vastaus[0] = in.nextLine();
+
+System.out.println("Sukunimi?");
+
+	vastaus[1] = in.nextLine();
+System.out.println("yrityksen verkkotunnus?");
+
+	vastaus[2] = in.nextLine();
+for (int i =0; i < vastaus.length ; i++) {
+	if(vastaus[i].equals("")) {
+		System.out.println("Virhe! Jokin tiedoista puuttui.");
+	}
+}
 	}
 
 }
